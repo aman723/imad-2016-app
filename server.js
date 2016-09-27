72d2e1239f9e290d03c9cfbf1cc25ce1f2d49e1a
aman723 <amannair723@gmail.com>
2016-09-27 17:51:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,11 +2,60 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 
+var aman1 ={
+    title: 'yoaman',
+    heading: 'mr j',
+    date: '27 sept 2016',
+    content: `<p>
+                is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true
+            </p>
+            <p>
+                is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true
+            </p>
+            <p>
+                is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true is it true
+            </p>`
+};
+function createinject(data){
+    var title=data.title;
+    var date=data.date;
+    var heading=data.heading;
+    var content=data.content;
+    var htmlinject=
+    `<html>
+    <head>
+        <title>
+        ${title}
+        </title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+    
+    </head>
+<body>
+    <div>
+        <a href="/aman2">yoamanyo</a>
+        </div>
+    <div>    <a href="/ui/style.css" rel="stylesheet">oamanyo</a></div>
+        <hr/>
+        <h3> 
+            ${heading}
+        </h3>
+        <div>
+            ${date}
+        </div>
+        <div>
+            ${content}
+            </div>
+        <a href="/">Home</a>
+    </body>        
+        </html>`;
+        return htmlinject;
+}
+
 var app = express();
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+  res.send(createinject(aman1));
 });
 
 app.get('/aman1', function (req, res) {
