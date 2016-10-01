2e162176eda56ef3edb116f57a28a663d09a5b1d
aman723 <amannair723@gmail.com>
2016-10-01 14:36:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,2 +1,12 @@
 console.log('Loaded!');
 console.log('ur about to read my app');
+var counter = doucment.getElementById('count');
+var count=0;
+counter.onclick=function(){
+count= count + 1;
+var span= doucment.getElementById('spamm');
+span.innerHTML = count.toString();
+};
+
+
+
