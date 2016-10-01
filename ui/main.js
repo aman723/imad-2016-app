795e7df15a2b29148065dd054eb3236656d92c4d
aman723 <amannair723@gmail.com>
2016-10-01 14:40:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,8 @@
 console.log('Loaded!');
 console.log('ur about to read my app');
-var counter = doucment.getElementById('count');
+var button = doucment.getElementById('count');
 var count=0;
-counter.onclick=function(){
+button.onclick=function(){
 count= count + 1;
 var span= doucment.getElementById('spamm');
 span.innerHTML = count.toString();
